5416ba13c46235df4cb4d305c186ac969f4ff8a8
Leonardo Vilarinho <leonardo-s@outlook.com>
2017-07-26 00:34:01 -0300

    debugando5

--- src/public/index.php
+++ src/public/index.php
@@ -19,10 +19,7 @@
 |
 */
 
-var_dump($_SERVER['MYSQLCONNSTR_localdb']);
-
-
-//require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +33,7 @@ var_dump($_SERVER['MYSQLCONNSTR_localdb']);
 |
 */
 
-// $app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -50,14 +47,14 @@ var_dump($_SERVER['MYSQLCONNSTR_localdb']);
 |
 */
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-// $response->send();
+$response->send();
 
-// $kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
 
 
